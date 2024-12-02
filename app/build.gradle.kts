33b16233b5f2c62def2cc16af6d3a9e279f65ed7
Kaiserdragon2 <8929967+Kaiserdragon2@users.noreply.github.com>
2024-12-02 17:27:20 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.kaiserdragon.weathershortcut"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
